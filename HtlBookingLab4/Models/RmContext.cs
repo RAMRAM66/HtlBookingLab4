f75228b80cf10f1bcbbf6b4f6fd8e4923788ea52
RAMRAM66 <ram-asou@mail.ru>
2020-11-16 01:29:41 +0300

    Add project files.

---
+++ HtlBookingLab4/Models/RmContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HtlBookingLab4.Models
+{
+    public class RmContext : DbContext
+    {
+        DbSet<RoomClass> RoomClasses { get; set; }
+        DbSet<Room> Room{ get; set; }
+    }
+}
---
+++ HtlBookingLab4/Models/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HtlBookingLab4.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public int RoomCount { get; set; }
+        public int BedCount { get; set; }
+        public int Price { get; set; }
+        public int? ClassId { get; set; }
+        public RoomClass RoomClass { get; set; }
+
+    }
+}
